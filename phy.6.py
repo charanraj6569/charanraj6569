0734ef7ccf39de3e63902efade9ba9b5fee9c103
charanraj6569 <163230204+charanraj6569@users.noreply.github.com>
2024-03-16 03:51:51 -0700

    Add files via upload

---
+++ phy.6.py
@@ -0,0 +1,303 @@
+16/03/24
+day:6
+s1 = "hellow world"
+print(s1.capitalize())
+
+
+s1= "hellow world"
+fst= s1[0].upper()
+lst= s1[-1].UPPER()
+
+print(fst+s1[1:len(s1)-1]+lst)
+
+n = 128
+while n!=0:
+    rem = n%10
+    print(rem)
+    n = n/10
+
+n = 128
+while n!=0:
+    rem = n%10
+    print("the value of rem:",rem)
+    n=n/10
+
+for val in range (len(l1)
+ans =l1 [l1[val]+l2[val]
+13.append(ans)
+print(13)         
+
+------>set
+
+Eg:1
+s1 = {9,9,89,8+7j,(8,7,5),"truck",'e'}
+print =(s1)
+
+s2 = {5,6,9,[9,0]}
+print(s2)
+
+min(),max(),len()
+
+min(),max(),len()
+
+s1 = {8,78,67,'u'}
+addas1.add(43)
+print(s1)
+
+-->update()
+s1.update([9,0])
+print(s1)
+
+-->update()
+s1.update([9,0])
+print(s1)
+
+To delete element inside set
+s1 = {8,9,7,'u'}
+
+remove()
+s1.remove(978)
+print(s1)
+
+
+
+discard()
+s1.discard(67)
+print(s1)
+
+
+clear()
+l1 = {}
+print(type(l1))-->datatype is dict
+
+s1 =set()---to  create empty set
+print(type(s1))
+
+
+join the sets
+s1 = {9,0,8}
+s2 = {9.90"card",'t',56}
+union()--->to combine 2 sets
+s3 = s1.union(s2)
+print(s3)
+
+inter section()-->to get commen element inside section set
+s1 = {4,5,6}
+s2 = {5,6,7,8}
+print(s1.intersection(s2))
+         
+s1 = {4,5,6}
+s2 = {5,6,7,8}
+print(s1.differance(s2))
+print(s2.differance(s1))
+print(s1.symmentric differance(s2))
+
+indisjoit(),issubset(),issuperset()
+
+s1  = {8,9,0}
+s2 ={2,7,5,8,9,0}
+
+print(s1.issubset(s2))
+print(s2.issuperset(s1))
+
+s1 = {1,2,3,4,5}
+s2 = {3,2,8,7,9}
+
+n1 = {1,2,3}--->
+
+for val in s1:
+    if val in s2:
+         strl = "its joint set"
+print(strl)
+
+o/p--->its a joint set
+---->dictionary
+Eg:1
+d1 = {1:100, 'a':200,4.5:"hello"}
+print(d1)
+print(len(d1))
+
+char of dictionary
+1.)have to be surrounded by {}
+2.)it have to be in the form of key,value pair
+3.)ity is mutable
+4.)duplicate key are not allowed,duplicate value are allowed
+5.)it is unindexe
+6.)it is ordered
+7.)key does not allowed mutable datatypes, value allow mutable datatype
+
+d1 = {1:100,2:200,3:300,4:400}
+-->to access element in dictionary
+
+print(d1)
+or
+to access the values
+print(d1[1])#o/p--->100
+
+-->some common functions
+len(),min(),max()
+print(min(d1))
+print(max(d1))
+
+to find min,max based on values
+print(min(d1.values()))
+print(max(d1.values()))
+
+dictionary based functions
+to add element(key and value pair)in dict
+d1 = {1:100,2:200,3:300,4:400}
+d1[5] = 500
+print(d1)
+
+get ()
+d1 = {1:100,2:200,3:300,4:400}
+print(d1[3])
+print(d1.get(3))
+
+to print all the keys
+print(d1.key())
+print(type(d1.values))
+
+iterating dictionaty
+for val in d1:
+    print(val)
+
+for val in d1.values:
+    print(val)
+
+to get the both key and values
+for key, val in d1.items():
+    print(key,val)
+    
+! problem:1
+n = int(input("Enter the number of times :"))
+integer=[]
+float_value=[]
+string=[]
+
+for val in range(n):
+    value=eval(input("enter the values:"))
+    if type (val)==int:
+        integer.append(val)
+    elif type(value)==flot:
+            flot_value.append(value)
+   elif type (value)==str:
+       string.append(value)
+   else:
+       print("pls provide data in int,flote,string")
+  print(integer)
+  print(flote_value)
+  print(string)
+  
+ return aset of elements present in set a or b
+set1={10,20,30,40,50}
+set2 = {30,40,50,60,70}
+o/p
+{20,70,10,60}
+
+swap elements in string list
+the original list is : ['Gfg','is','best'
+list after performing charecter swaps:['efg 
+
+
+                        
+d1= {}
+for val in range(len(11)):
+    d1[l1[val]]=l2[val]
+print(d1)
+                 
+
+                 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+         
+
+         
+         
+
+         
+         
+         
+         
+         
+         
+         
+         
+         
+
+         
+
+         
+                  
+         
+
+
+
+
+         
+         
+         
+         
+         
+         
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+         
+
+         
+
+         
+
+
+
+
+         
+
+         
+         
+
+         
